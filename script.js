637858e67d10af8f977bed8ca491812c476cdcbc
Joseph Abbey <abbeyjoephotos@gmail.com>
2026-07-25 21:13:25 -0400

    Pause/Resume function added

--- script.js
+++ script.js
@@ -15,7 +15,10 @@ const yellowButton =
     document.querySelector("#yellow-button");
 
 const greenButton =
-    document.querySelector("#green-button")
+    document.querySelector("#green-button");
+
+const buttons =
+    [redButton,yellowButton, greenButton];
 
 const lightChoiceMap = {
     red: redLight,
@@ -26,15 +29,26 @@ const lightChoiceMap = {
 const choices = [redLight, yellowLight, greenLight];
 
 let timer;
+let timerStart;
+let remainingTime;
+let currentDuration;
+
+let isPaused = false;
+let currentLight = null;
+
 // Functions for lighting up //
 
 function selectRed() {
+    currentLight = "red";
     clearTimeout(timer);
+    currentDuration = 10000;
 
     yellowLight.classList.remove("on");
     greenLight.classList.remove("on");
 
     redLight.classList.add("on");
+
+    timerStart = Date.now();
 
     timer = setTimeout(() => {
         selectGreen();
@@ -42,12 +56,16 @@ function selectRed() {
 };
 
 function selectYellow() {
+    currentLight = "yellow";
     clearTimeout(timer);
+    currentDuration = 2000
 
     redLight.classList.remove("on");
     greenLight.classList.remove("on");
 
     yellowLight.classList.add("on");
+
+    timerStart = Date.now();
 
     timer = setTimeout(() => {
         selectRed();
@@ -55,33 +73,107 @@ function selectYellow() {
 };
 
 function selectGreen() {
+    currentLight = "green";
     clearTimeout(timer);
+    currentDuration = 10000;
 
     yellowLight.classList.remove("on");
     redLight.classList.remove("on");
 
     greenLight.classList.add("on");
 
+    timerStart = Date.now();
+
     timer = setTimeout(() => {
         selectYellow();
     }, 10000);
 };
 
+function pauseTimer() {
+    let elapsedTime = Date.now() - timerStart;
+
+    remainingTime = currentDuration - elapsedTime;
+    clearTimeout(timer);
+
+    if (currentLight === "red") {
+        redButton.classList.add("paused");
+    }
+    else if (currentLight === "yellow") {
+        yellowButton.classList.add("paused");
+    }
+    else {
+        greenButton.classList.add("paused");
+    }
+
+    isPaused = true;
+}
+
+function resumeTimer() {
+    currentDuration = remainingTime;
+
+    timerStart = Date.now();
+    
+    if (currentLight === "red") {
+        timer = setTimeout(() => { 
+            selectGreen();
+        }, remainingTime);
+
+        redButton.classList.remove("paused");
+    }
+    else if (currentLight === "green") {
+        timer = setTimeout(() => {
+            selectYellow();
+        },remainingTime);
+
+        greenButton.classList.remove("paused");
+    }
+    else {
+        timer = setTimeout(() => {
+            selectRed();
+        },remainingTime);
+
+        yellowButton.classList.remove("paused");
+    }
+    
+    
+    isPaused = false;
+}
 // Event listener //
-redButton.addEventListener("click", function() {
-    selectRed();
-});
 
-yellowButton.addEventListener("click", function() {
-    selectYellow();
-});
-greenButton.addEventListener("click", function() {
-    selectGreen();
-});
+buttons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+        const clickedButton = event.target.id;
+        let clickedLight;
 
+        if (clickedButton === "red-button") {
+            clickedLight = "red";           
+        }
+        else if (clickedButton === "yellow-button") {
+            clickedLight = "yellow";
+        }
+        else {
+            clickedLight = "green";
 
-timer = setTimeout(() => {
-    console.log("Times up!");
-}, 3000);
-clearTimeout(timer);
-console.log("timer cancelled")
+        }
+        if (currentLight === clickedLight) {
+            if (isPaused) {
+                resumeTimer();
+            }
+            else {
+                pauseTimer();
+            }
+        }
+        else {
+            if (clickedLight === "red") {
+                selectRed();
+            }
+            else if (clickedLight === "yellow") {
+                selectYellow();
+            }
+            else {
+                selectGreen();
+            }
+
+        };
+    });
+});
