6976720e68c017f459ae5e53b7b419d785773de0
Joseph Abbey <abbeyjoephotos@gmail.com>
2026-07-29 19:34:29 -0400

    Timer logic working and added detail to lights

--- script.js
+++ script.js
@@ -28,7 +28,29 @@ const lightChoiceMap = {
 
 const choices = [redLight, yellowLight, greenLight];
 
+// Timer display //
+
+const digitsMap = {
+    0: ["top", "upper-left","upper-right", "bottom-left", "bottom-right", "bottom"],
+    1: ["upper-right","bottom-right"],
+    2: ["top", "upper-right", "middle", "bottom-left", "bottom"],
+    3: ["top", "upper-right", "middle", "bottom-right", "bottom"],
+    4: ["upper-left", "upper-right", "middle", "bottom-right"],
+    5: ["top", "upper-left", "middle", "bottom-right", "bottom"],
+    6: ["top", "upper-left", "middle", "bottom-left", "bottom-right", "bottom"],
+    7: ["top", "upper-right", "bottom-right"],
+    8: ["top", "upper-left", "upper-right", "middle", "bottom-left", "bottom-right", "bottom"],
+    9: ["top", "upper-left", "upper-right", "middle", "bottom-right", "bottom"]
+};
+
+const tens = document.querySelector("#tens");
+const ones = document.querySelector("#ones");
+const milliseconds = document.querySelector("#milliseconds");
+
+// Variables //
 let timer;
+let timeInterval;
+
 let timerStart;
 let remainingTime;
 let currentDuration;
@@ -36,60 +58,26 @@ let currentDuration;
 let isPaused = false;
 let currentLight = null;
 
-// Functions for lighting up //
+// Functions for lighting up  and timer //
 
-function selectRed() {
-    currentLight = "red";
-    clearTimeout(timer);
-    currentDuration = 10000;
+function displayDigit(display, digit) {
+    const segments = display.querySelectorAll(".segment");
+    const activeSegments = digitsMap[digit];
 
-    yellowLight.classList.remove("on");
-    greenLight.classList.remove("on");
+    segments.forEach((segment) => {
+        const position = segment.classList[1];
 
-    redLight.classList.add("on");
-
-    timerStart = Date.now();
-
-    timer = setTimeout(() => {
-        selectGreen();
-    }, 10000);
-};
-
-function selectYellow() {
-    currentLight = "yellow";
-    clearTimeout(timer);
-    currentDuration = 2000
-
-    redLight.classList.remove("on");
-    greenLight.classList.remove("on");
-
-    yellowLight.classList.add("on");
-
-    timerStart = Date.now();
-
-    timer = setTimeout(() => {
-        selectRed();
-    }, 2000);
-};
-
-function selectGreen() {
-    currentLight = "green";
-    clearTimeout(timer);
-    currentDuration = 10000;
-
-    yellowLight.classList.remove("on");
-    redLight.classList.remove("on");
-
-    greenLight.classList.add("on");
-
-    timerStart = Date.now();
-
-    timer = setTimeout(() => {
-        selectYellow();
-    }, 10000);
+        if (activeSegments.includes(position)) {
+            segment.classList.add("on");
+        } else {
+            segment.classList.remove("on");
+        }
+    });
 };
 
 function pauseTimer() {
+    clearTimeout(timer);
+    clearInterval(timeInterval);
 
     let elapsedTime = Date.now() - timerStart;
 
@@ -107,7 +95,7 @@ function pauseTimer() {
     }
 
     isPaused = true;
-}
+};
 
 function resumeTimer() {
     currentDuration = remainingTime;
@@ -136,9 +124,94 @@ function resumeTimer() {
         yellowButton.classList.remove("paused");
     }
     
+    startCountdown();
     
     isPaused = false;
+};
+
+function startCountdown() {
+    clearInterval(timeInterval);
+
+    timeInterval = setInterval(() => {   
+        let elapsedTime = Date.now() - timerStart;
+        remainingTime = currentDuration - elapsedTime;
+
+        let millisecondsDigit = Math.floor(remainingTime / 100) % 10;
+        displayDigit(milliseconds, millisecondsDigit);
+
+        let seconds = Math.floor(remainingTime / 1000);
+
+        let onesDigit = seconds % 10;
+        displayDigit(ones, onesDigit);
+        
+        let tensDigit = Math.floor(seconds / 10);
+        displayDigit(tens, tensDigit);
+    }, 100);
 }
+
+
+function selectRed() {
+    currentLight = "red";
+    clearTimeout(timer);
+
+    currentDuration = 10000;
+
+    yellowLight.classList.remove("on");
+    greenLight.classList.remove("on");
+
+    redLight.classList.add("on");
+
+    timerStart = Date.now();
+
+    startCountdown();
+
+    timer = setTimeout(() => {
+        selectGreen();
+    }, 10000);
+};
+
+function selectYellow() {
+    currentLight = "yellow";
+    clearTimeout(timer);
+
+    currentDuration = 2000
+
+    redLight.classList.remove("on");
+    greenLight.classList.remove("on");
+
+    yellowLight.classList.add("on");
+
+    timerStart = Date.now();
+
+    startCountdown();
+
+    timer = setTimeout(() => {
+        selectRed();
+    }, 2000);
+};
+
+function selectGreen() {
+    currentLight = "green";
+    clearTimeout(timer);
+
+    currentDuration = 10000;
+
+    yellowLight.classList.remove("on");
+    redLight.classList.remove("on");
+
+    greenLight.classList.add("on");
+
+    timerStart = Date.now();
+
+    startCountdown();
+
+    timer = setTimeout(() => {
+        selectYellow();
+    }, 10000);
+};
+
+
+
 // Event listener //
 
 buttons.forEach((button) => {
